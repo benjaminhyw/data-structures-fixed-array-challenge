9cb6b6fa1c61a295b5b12653a7249967d87ecaa3
Ben Flores <benjaminhyw@gmail.com>
2016-08-02 16:31:12 -0700

    Create initialize method for FixedArray

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,16 @@
 class FixedArray
+  attr_accessor :size
 
+  def initialize(size)
+    @size = size
+    @array = Array.new(size)
+  end
+
+  def get(index)
+    #OutOfBoundsException if user tries to get a value at an index outside the bounds of the fixed array
+  end
+
+  def set(index,element)
+    #OutOfBoundsException if user tries to set a value at an index outside the bounds of the fixed array
+  end
 end
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,11 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
+  describe "new" do
+
+    it "initializes an array with the size parameter provided" do
+      expect(FixedArray.new(4).size).to eq 4
+    end
+  end
   
 end
